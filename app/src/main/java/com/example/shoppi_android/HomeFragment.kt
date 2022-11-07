6afc87b568d8c870bcaca7420e470819d342fdba
안정은 <jjeee1313@gmail.com>
2022-11-07 21:08:30 +0900

    섹션5. Splash 화면 추가, 앱 호환성 대응, 화면 연결

---
+++ app/src/main/java/com/example/shoppi_android/HomeFragment.kt
@@ -0,0 +1,32 @@
+package com.example.shoppi_android
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.shoppi_android.databinding.FragmentHomeBinding
+
+class HomeFragment : Fragment() {
+
+    private lateinit var binding: FragmentHomeBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding = FragmentHomeBinding.bind(view)
+        binding.btnEnterProductDetail.setOnClickListener {
+            val transaction = parentFragmentManager.beginTransaction()
+            transaction.add(R.id.container_main, ProductDetailFragment())
+            transaction.commit()
+        }
+    }
+}
--- app/src/main/java/com/example/shoppi_android/MainActivity.kt
+++ app/src/main/java/com/example/shoppi_android/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.example.shoppi_android
 
+import android.content.ContentValues.TAG
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+
+private const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_product_detail)
+        setContentView(R.layout.activity_main)
     }
 }
